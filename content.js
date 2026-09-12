bd7dbc0798e448f715fadbcc70e3187095d0732a
AstraStasis <geanandreialsonado@gmail.com>
2026-09-12 23:48:04 +0800

    yt_autoskipbutton

---
+++ content.js
@@ -0,0 +1,15 @@
+function skipAd() {
+    const video = document.querySelector('video');
+    const isAdPlaying = document.querySelector('.ad-showing, .ad-interrupting');
+    
+
+    if (video && isAdPlaying) {
+        if (isFinite(video.duration)) {
+            video.currentTime = video.duration - 0.1;
+            console.log("Auto-Skipper: Ad fast-forwarded!");
+        }
+    }
+}
+
+
+setInterval(skipAd, 500);
